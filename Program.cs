bb0ecbc0736a4b30db973e906041f5bb52acf44c
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-03-21 19:49:05 -0300

    Update base files

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using System.Text;
+using card_game_manager_service_user;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var key = Encoding.ASCII.GetBytes(Settings.Secret);
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
